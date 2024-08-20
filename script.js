1152a59fab58f43bc73c771947e8fb3750bbf16e
MohannadAlmageedi <157972818+MohannadAlmageedi@users.noreply.github.com>
2024-08-20 19:29:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("downloadButton").addEventListener("click", function () {
+    window.location.href = "https://mega.nz/file/Bhw1AbRB#mzElpoPJmpIvUMBoFUIicOf3QnHd3aQIHqMVH0_Bx7Y";
+});
